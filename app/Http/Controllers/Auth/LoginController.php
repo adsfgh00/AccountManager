0b1ecdc28aa3b604e0f2a16599388812794f65b4
Mark <adsfgh00@gmail.com>
2019-04-22 18:10:19 +0900

    sample

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Auth;
+use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     /*
@@ -32,8 +33,16 @@ class LoginController extends Controller
      *
      * @return void
      */
+    public function logout(Request $request) {
+      Auth::logout();
+      return redirect('/login');
+    }
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function username()
+    {
+        return('username');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::get('/login', function () {
 });
 
 Auth::routes();
-
+Route::get('logout', 'LoginController@logout');
 Route::get('/home', 'HomeController@index')->name('home');
